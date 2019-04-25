5a366229d4ef39b1489a4b4671f59e2c8a988453
chentao <529953602@qq.com>
2019-04-25 20:39:56 +0800

    first commit

---
+++ lib/index/index.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'navigation_icon_view.dart';
+
+class Index extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return null;
+  }
+}
