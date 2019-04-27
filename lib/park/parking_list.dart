2ac8f72ee81a5b4134484209f3cb7c5ca75a1daf
chentao <529953602@qq.com>
2019-04-27 13:16:20 +0800

    modify

--- lib/park/parking_list.dart
+++ lib/park/parking_list.dart
@@ -11,33 +11,47 @@ class _ParkingListState extends State<ParkingList> {
     return new Container(
       /// 卡片包装
       child: new Card(
-
-          /// 增加点击效果
-          child: new FlatButton(
-              onPressed: () {
-                print("点击了List");
-              },
-              child: new Padding(
-                padding: new EdgeInsets.only(
-                    left: 0.0, top: 0.0, right: 10.0, bottom: 10.0),
-                child: new Column(
-                  mainAxisSize: MainAxisSize.max,
-                  children: <Widget>[
-                    ///文本描述
-                    new Container(
-                        child: new Text(
-                          "浦口区图书馆(浦口分馆)",
-                          style: TextStyle(
-                            color: Color(0xff959595),
-                            fontSize: 14.0,
-                          ),
-                        ),
-                        margin: new EdgeInsets.only(top: 6.0, bottom: 2.0),
-                        alignment: Alignment.topLeft),
-                    new Padding(padding: EdgeInsets.all(10.0)),
-                  ],
-                ),
-              ))),
+          child: new Padding(
+        padding: new EdgeInsets.only(
+            left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
+        child: new Row(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            new Container(
+              child: new Icon(Icons.edit_location),
+              padding: EdgeInsets.only(right: 10.0),
+            ),
+            new Expanded(
+              child: new Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                children: <Widget>[
+                  new Container(
+                    child: new Text(
+                      "浦口区图书馆(浦口分馆)",
+                      style: TextStyle(fontSize: 15.0),
+                    ),
+                    padding: EdgeInsets.only(bottom: 5.0),
+                  ),
+                  new Container(
+                    child: new Text(
+                      "江苏省-南京市-浦口区-新马路3号",
+                      style: TextStyle(fontSize: 10.0),
+                    ),
+                  )
+                ],
+              ),
+//              padding: EdgeInsets.only(left: 10.0),
+            ),
+            new Container(
+              child: new Column(
+                children: <Widget>[new Icon(Icons.drive_eta), new Text("路线")],
+              ),
+            )
+          ],
+        ),
+      )),
     );
   }
 }
